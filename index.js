23ff14eb2099d7b99870fc8686048ed3185b495e
ItNairi <37809835+Ricardo0697@users.noreply.github.com>
2021-11-09 22:46:46 -0600

    se completa el webScraping en pagina se devuelve informacion requerida

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const PORT = 8000
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
+
+const app = express()
+const URLONE = 'https://www.cso.go.cr/legislacion/criterios_tecnicos.aspx';
+axios(URLONE)
+    .then(response => {
+        const html = response.data
+        //    console.log(html)
+        const $ = cheerio.load(html)
+        const articles = []
+        $('.REPEATERROW', html).each(function () {
+            const text = $(this).text()
+            // const tag = $(this).find('tr').attr()
+            articles.push({ text })
+        })
+        console.log(articles , articles.length)
+    }).catch(err => console.log(err, 'error al intentar Ingresar a la web'))
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
